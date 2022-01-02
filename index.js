220e4608f0e9335d80ba4e50e64e5f842015d124
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-01-02 21:11:40 +0700

    fix: reduce method

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const sum = (values = []) => {
-  return values.reduce((prev, cur) => prev + cur, 0);
+  return values.reduce((prev, cur) => prev + cur);
 };
 
 const subtract = (values = []) => {
-  return values.reduce((prev, cur) => prev - cur, 0);
+  return values.reduce((prev, cur) => prev - cur);
 };
 
 const multiply = (values = []) => {
-  return values.reduce((prev, cur) => prev * cur, 0);
+  return values.reduce((prev, cur) => prev * cur);
 };
 
 const divide = (values = []) => {
-  return values.reduce((prev, cur) => prev / cur, 0);
+  return values.reduce((prev, cur) => prev / cur);
 };
 
 module.exports = {
