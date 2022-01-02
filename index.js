e606e0e6d96e08d8692095ba78bb343d00ba852b
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-01-02 21:07:34 +0700

    add more tests and math function

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const sum = (values = []) => {
   return values.reduce((prev, cur) => prev + cur, 0);
 };
 
+const subtract = (values = []) => {
+  return values.reduce((prev, cur) => prev - cur, 0);
+};
+
+const multiply = (values = []) => {
+  return values.reduce((prev, cur) => prev * cur, 0);
+};
+
+const divide = (values = []) => {
+  return values.reduce((prev, cur) => prev / cur, 0);
+};
+
 module.exports = {
   sum,
+  subtract,
+  multiply,
+  divide,
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,22 @@
 const { describe, it } = require("mocha");
 const assert = require("assert");
-const { sum } = require("../index.js");
+const { sum, subtract, divide, multiply } = require("../index.js");
 
 describe("Test Math Functions", () => {
   it("Test sum function", () => {
     assert.equal(sum([1, 2, 5]), 8);
     assert.notEqual(sum([1, 1]), 4);
+  });
+  it("Test subtract function", () => {
+    assert.equal(subtract([10, 5]), 5);
+    assert.notEqual(subtract([10, 1]), 8);
+  });
+  it("Test multiply function", () => {
+    assert.equal(multiply([10, 5]), 50);
+    assert.notEqual(multiply([10, 1]), 20);
+  });
+  it("Test divide function", () => {
+    assert.equal(divide([10, 5]), 2);
+    assert.notEqual(divide([10, 1]), 0);
   });
 });
